9a0b8fb38f93c937f7fc8e610c3df3357256bd14
tapphughesn <nicholas.tapphughes@gmail.com>
2020-04-30 01:04:41 -0400

    Dataset is too large, cant handle it

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import datetime
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,49 +32,59 @@ model.compile(optimizer='adam',
 model_checkpoint = keras.callbacks.ModelCheckpoint('checkpoints', monitor="loss", verbose=0)
 
 # train model on sections of the dataset
+# There are approximately 10,000 images in the training dataset
+# Train on sections of the dataset so it can fit in my GPU memory (8GB)
 
-    train_imgs_0 = []
-    train_labels_0 = []
-    
-    for f in glob('DATA/TRAIN/EOSINOPHIL/*'):
-        im = np.asarray(Image.open(f))
-        train_imgs_0.append(im)
-        train_labels_0.append(0)
-    
-    for f in glob('DATA/TRAIN/LYMPHOCYTE/*'):
-        im = np.asarray(Image.open(f))
-        train_imgs_0.append(im)
-        train_labels_0.append(1)
-    
-    for f in glob('DATA/TRAIN/MONOCYTE/*'):
-        im = np.asarray(Image.open(f))
-        train_imgs_0.append(im)
-        train_labels_0.append(2)
-    
-    for f in glob('DATA/TRAIN/NEUTROPHIL/*'):
-        im = np.asarray(Image.open(f))
-        train_imgs_0.append(im)
-        train_labels_0.append(3)
-    
-    # Shuffle the dataset and labels with the same shuffle
-    permutation = np.arange(len(train_imgs_0))
-    np.random.shuffle(permutation)
-    train_imgs = []
-    train_labels = []
-    for i in range(len(train_imgs_0)):
-        index = permutation[i]
-        train_imgs.append(train_imgs_0[index])
-        train_labels.append(train_labels_0[index])
-    
-    del train_imgs_0
-    del train_labels_0
-    train_imgs = np.asarray(train_imgs).astype(np.float16)
-    train_labels = np.asarray(train_labels).astype(np.float16)
-    print("Imgs size: ", sys.getsizeof(train_imgs))
-    print("Labels size: ", sys.getsizeof(train_labels))
-    train_imgs = tf.convert_to_tensor(train_imgs, dtype=tf.float16)
-    train_labels = tf.convert_to_tensor(train_labels, dtype=tf.float16)
-    
-    
-    # use Model.fit() to train the model
-    model.fit(train_imgs, train_labels, batch_size=4, epochs=1, verbose=1, callbacks=[model_checkpoint], shuffle=False)
+train_imgs_0 = []
+train_labels_0 = []
+
+for f in glob('DATA/TRAIN/EOSINOPHIL/*'):
+    im = np.asarray(Image.open(f)).astype(np.float16)
+    train_imgs_0.append(im)
+    train_labels_0.append(0)
+
+for f in glob('DATA/TRAIN/LYMPHOCYTE/*'):
+    im = np.asarray(Image.open(f)).astype(np.float16)
+    train_imgs_0.append(im)
+    train_labels_0.append(1)
+
+for f in glob('DATA/TRAIN/MONOCYTE/*'):
+    im = np.asarray(Image.open(f)).astype(np.float16)
+    train_imgs_0.append(im)
+    train_labels_0.append(2)
+
+for f in glob('DATA/TRAIN/NEUTROPHIL/*'):
+    im = np.asarray(Image.open(f)).astype(np.float16)
+    train_imgs_0.append(im)
+    train_labels_0.append(3)
+
+# Shuffle the dataset and labels with the same shuffle
+permutation = np.arange(len(train_imgs_0))
+np.random.shuffle(permutation)
+train_imgs = []
+train_labels = []
+for i in range(len(train_imgs_0)):
+    index = permutation[i]
+    train_imgs.append(train_imgs_0[index])
+    train_labels.append(train_labels_0[index])
+
+del train_imgs_0
+del train_labels_0
+train_labels = np.asarray(train_labels).astype(np.float16)
+print("Imgs size: ", sys.getsizeof(train_imgs))
+print("Labels size: ", sys.getsizeof(train_labels))
+
+print(tf.shape(train_imgs))
+print(tf.shape(train_labels))
+
+# use Model.fit() to train the model
+batch_size = 100
+num_batches = len(train_imgs) // batch_size
+for batch in range(len(num_batches)):
+    if (((batch+1)*batch_size) > len(train_imgs)):
+        break
+    model.fit(train_imgs[batch], train_labels[batch], batch_size=4, epochs=1, verbose=1, callbacks=[model_checkpoint], shuffle=False)
+
+# Save Model weights
+model.save_weights('checkpoints/' + datetime.now().strftime("%m-%d-%Y-%H:%M:%S") + "_parameters")
+
