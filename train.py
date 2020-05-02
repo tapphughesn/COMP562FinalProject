03af7929dce9ca88371e3ed24df0d1bdc1b16d0d
Nick Tapp-Hughes <nicholas.tapphughes@gmail.com>
2020-05-02 14:25:27 -0400

    training on longleaf

--- train.py
+++ train.py
@@ -8,22 +8,21 @@ import tensorflow as tf
 from tensorflow import keras
 import os
 
-#Stop tensorflow from seeing my gpu bc I can't get it to use my gpu correctly anyway
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
 # Model Structure
 model = keras.Sequential([
+    keras.layers.Conv2D(4, 3, padding = 'same', activation='relu'),
     keras.layers.Conv2D(8, 3, padding = 'same', activation='relu'),
+    keras.layers.MaxPool2D(pool_size = 2, padding = 'same'), 
+    keras.layers.Conv2D(16, 3, padding = 'same', activation='relu'),
     keras.layers.Conv2D(16, 3, padding = 'same', activation='relu'),
     keras.layers.MaxPool2D(pool_size = 2, padding = 'same'), 
     keras.layers.Conv2D(32, 3, padding = 'same', activation='relu'),
+    keras.layers.Conv2D(32, 3, padding = 'same', activation='relu'),
+    keras.layers.MaxPool2D(pool_size = 2, padding = 'same'), 
+    keras.layers.Conv2D(64, 3, padding = 'same', activation='relu'),
     keras.layers.Conv2D(64, 3, padding = 'same', activation='relu'),
     keras.layers.MaxPool2D(pool_size = 2, padding = 'same'), 
-    keras.layers.Conv2D(128, 3, padding = 'same', activation='relu'),
-    keras.layers.Conv2D(256, 3, padding = 'same', activation='relu'),
-    keras.layers.MaxPool2D(pool_size = 2, padding = 'same'), 
-    keras.layers.Flatten(input_shape=(30,40,3)),
-    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Flatten(input_shape=(15,20,3)),
     keras.layers.Dense(4)
     ])
 
@@ -32,14 +31,12 @@ model.compile(optimizer='adam',
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics = ['accuracy'])
 
-# train model on sections of the dataset
-# There are approximately 10,000 images in the training dataset
-# For some reason data can't exceed 10% of my system memory (16GB -> 1.6GB)
 
 train_imgs_0 = []
 train_labels_0 = []
 
-file_names = glob('DATA/TRAIN/EOSINOPHIL/*') + glob('DATA/TRAIN/LYMPHOCYTE/*') + glob('DATA/TRAIN/MONOCYTE/*') + glob('DATA/TRAIN/NEUTROPHIL/*')
+train_file_names = glob('DATA/TRAIN/EOSINOPHIL/*') + glob('DATA/TRAIN/LYMPHOCYTE/*') + glob('DATA/TRAIN/MONOCYTE/*') + glob('DATA/TRAIN/NEUTROPHIL/*')
+test_file_names = glob('DATA/TEST/EOSINOPHIL/*') + glob('DATA/TEST/LYMPHOCYTE/*') + glob('DATA/TEST/MONOCYTE/*') + glob('DATA/TEST/NEUTROPHIL/*')
 
 # Make a dictionary mapping the first letter of the cell type to the label integer
 class_dict = {}
@@ -49,12 +46,13 @@ class_dict['M'] = 2
 class_dict['N'] = 3
 
 # Shuffle file names to mix up order of training
-np.random.shuffle(file_names)
+np.random.shuffle(train_file_names)
+np.random.shuffle(test_file_names)
 
 # Make train_imgs and train_labels for this section
 train_imgs = []
 train_labels = []
-for f in file_names:
+for f in train_file_names:
     im = np.asarray(Image.open(f)).astype(np.float16)
     train_imgs.append(im)
     # 11th letter in file name is the first letter of the cell type
@@ -64,9 +62,31 @@ for f in file_names:
 train_imgs = np.asarray(train_imgs)
 train_labels = np.asarray(train_labels).astype(np.float16)
 
+# Make train_imgs and train_labels for this section
+test_imgs = []
+test_labels = []
+for f in test_file_names:
+    im = np.asarray(Image.open(f)).astype(np.float16)
+    test_imgs.append(im)
+    # 10th letter in test file name is the first letter of the cell type
+    label = class_dict[f[10]]
+    test_labels.append(3)
+
+test_imgs = np.asarray(test_imgs)
+test_labels = np.asarray(test_labels).astype(np.float16)
+
+# Create checkpoint to save weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath="checkpoints/cp_" + datetime.now().strftime("%m-%d-%Y-%H:%M:%S") + ".ckpt",
+        save_weights_only = True,
+        verbose = 1)
+
 # use Model.fit() to train the model
-model.fit(train_imgs, train_labels, epochs=1, verbose=1, shuffle=False)
+model.fit(train_imgs, train_labels, validation_data=(test_imgs, test_labels), epochs=1, verbose=1, callbacks=[cp_callback], shuffle=False)
+
+# print model summary for information abt the model
+print(model.summary())
      
 # Save Model weights
-model.save_weights('checkpoints/' + datetime.now().strftime("%m-%d-%Y-%H:%M:%S") + "_parameters")
+# model.save_weights('checkpoints/' + datetime.now().strftime("%m-%d-%Y-%H:%M:%S") + "_parameters")
      
